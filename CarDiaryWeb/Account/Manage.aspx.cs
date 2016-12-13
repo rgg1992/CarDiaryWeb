ebbbf27b210e660623c354c09873012f2018b679
Radoslav.Gavrailov <Radoslav.Gavrailov@gmail.com>
2016-12-13 10:28:36 +0200

    Modify Manage.aspx.cs file due to a compilation error

--- CarDiaryWeb/Account/Manage.aspx.cs
+++ CarDiaryWeb/Account/Manage.aspx.cs
@@ -128,5 +128,45 @@ namespace CarDiaryWeb.Account
 
             Response.Redirect("/Account/Manage");
         }
+
+
+        protected void ChangePassword_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
+                IdentityResult result = manager.ChangePassword(User.Identity.GetUserId(), CurrentPassword.Text, NewPassword.Text);
+                if (result.Succeeded)
+                {
+                    var user = manager.FindById(User.Identity.GetUserId());
+                    signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
+                    Response.Redirect("~/Account/Manage?m=ChangePwdSuccess");
+                }
+                else
+                {
+                    AddErrors(result);
+                }
+            }
+        }
+
+        protected void SetPassword_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                // Create the local login info and link the local account to the user
+                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                IdentityResult result = manager.AddPassword(User.Identity.GetUserId(), password.Text);
+                if (result.Succeeded)
+                {
+                    Response.Redirect("~/Account/Manage?m=SetPwdSuccess");
+                }
+                else
+                {
+                    AddErrors(result);
+                }
+            }
+        }
+
     }
 }
